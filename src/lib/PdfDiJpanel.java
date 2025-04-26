149c997337a33538cd038873cb332548e23e9b40
lanaSaja <it.maulanaali@gmail.com>
2025-04-26 19:01:44 +0700

    tambah lib filedipanel dan chossefile

---
+++ src/lib/PdfDiJpanel.java
@@ -0,0 +1,24 @@
+package lib;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+//Class untuk menampilkan file pdf a4 di jpanel
+public class PdfDiJpanel {
+    private PDDocument pdfFile;
+    private PDFRenderer image;
+    
+    public PdfDiJpanel addFile(byte[] fileBloob) throws Exception{
+        this.pdfFile = PDDocument.load(new ByteArrayInputStream(fileBloob));
+        return this;
+    }
+    
+    public ImageIcon getIcon() throws Exception{
+        BufferedImage fotoSurat = this.image.renderImageWithDPI(0, 150);
+        return new ImageIcon(fotoSurat);
+    }
+}
---
+++ src/lib/PilihFile.java
@@ -0,0 +1,37 @@
+package lib;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
+public class PilihFile {
+    
+    public static String getPath(){
+        String pathFile = "";
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Pilih file");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(".pdf", "pdf");
+        fileChooser.setFileFilter(filter);
+
+        int result = fileChooser.showOpenDialog(null);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            pathFile = selectedFile.getAbsolutePath();
+            return pathFile;
+        }
+        return pathFile;
+    }
+}
